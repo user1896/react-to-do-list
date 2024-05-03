4a502e3488cf10dbdc82f5c4f2af4efe6aa52247
user1896 <abde.user15@gmail.com>
2024-05-03 07:52:42 +0200

    add tailwind to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-sky-600 flex justify-center items-center min-h-screen">
       <Main />
     </div>
   );
